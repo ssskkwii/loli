b4d1f5c6ea59c58207f4de92afa0fcb4782c1012
lo-li-pop <84183369+ssskkwii@users.noreply.github.com>
2021-07-05 21:32:52 +0300

    hello
    the person who wrote the code lo-li-pop
    discord: ssscute#0001

---
+++ loli.py
@@ -0,0 +1,7 @@
+def chat():
+        nickname = input('nickname enter:')
+        passwork = input('passwork enter:')
+        print(nickname)
+        chat = input('message enter:')
+        print(chat)
+chat()
